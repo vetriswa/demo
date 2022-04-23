6283c94707e70bad4981e76a7cdc113d3dc82faf
vetriswa <vetriswa@gmail.com>
2022-04-23 11:02:13 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('i am in the middleware !');
+    next();
+});
+app.use((req, res, next) => {
+    console.log('in another middleware !');
+    res.send('<h1>hello from express</h1>');
+});
+
+
+//const server = http.createServer(app);
+
+//server.listen(3000);
+
+app.listen(3000);
