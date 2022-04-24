b21341f58b2ec7f1b09814a236f127776dc8f22e
vetriswa <vetriswa@gmail.com>
2022-04-23 21:14:56 -0700

    middle

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const http = require('http');
 
 const express = require('express');
-
 const app = express();
-
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    console.log('i am in the middleware !');
+    console.log('This always run');
     next();
 });
-app.use((req, res, next) => {
+
+app.use('/add-product', (req, res, next) => {
+    console.log('in another middleware !');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add product</button></form>');
+});
+app.use('/product', (req, res, next) => {
+    console.log('req.body');
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     console.log('in another middleware !');
     res.send('<h1>hello from express</h1>');
 });
