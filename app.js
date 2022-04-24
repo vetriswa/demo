e2b770883e2b318794209ac8d10329ff570f38a5
vetriswa <vetriswa@gmail.com>
2022-04-24 11:33:18 -0700

    shop,admin

--- app.js
+++ app.js
@@ -1,31 +1,17 @@
-const http = require('http');
-
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const app = express();
+
+const adminRoutes = require('./admin');
+const shopRoutes = require('./shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    console.log('This always run');
-    next();
+    res.status(404).send('<h1>page not found</h1>');
 });
-
-app.use('/add-product', (req, res, next) => {
-    console.log('in another middleware !');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit"> Add product</button></form>');
-});
-app.use('/product', (req, res, next) => {
-    console.log('req.body');
-    res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-    console.log('in another middleware !');
-    res.send('<h1>hello from express</h1>');
-});
-
-
-//const server = http.createServer(app);
-
-//server.listen(3000);
 
 app.listen(3000);
--- message.js
+++
@@ -1,4 +0,0 @@
-const http = require('http');
-const route = require('./route');
-const server = http.createServer(route.handler);
-server.listen(3000)
